258b279843b3b8389b6754b8818160b91eed0ac0
Alphons Jaimon <jaimonalphons@gmail.com>
2024-02-13 18:32:53 +0530

    Update script and interpreter in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     apps : [{
       name: 'vector-stay-awake-job',
-      script: 'nvm install 20; npm install; node schedule.js', // Replace with the path to your script
+      script: 'schedule.js', // Replace with the path to your script
       instances: 1,
       autorestart: false,
       watch: false,
+      exec_mode: 'fork',
       cron_restart: '0 0 * * 0', // This cron pattern means "At 00:00 on Sunday"
+      interpreter: 'node@20.11.0',
     }]
   };
-  
